6476e3407f3d8e871ca71e64087afb674e7093b6
Denis_Valuev <valuev.denis91@mail.ru>
2024-11-14 11:01:17 +0300

    Добавьте файлы проекта.

---
+++ SF.HW03.MD17/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SF.HW03.MD17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
